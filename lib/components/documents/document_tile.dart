218ae73ea0f79a44e2b61a38e1d3105b646319a5
mateuszanella <mateusblk1@gmail.com>
2024-09-26 19:53:39 -0300

    #4 Document click animation

--- lib/components/documents/document_tile.dart
+++ lib/components/documents/document_tile.dart
@@ -1,17 +1,37 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class DocumentTile extends StatelessWidget {
+class DocumentTile extends StatefulWidget {
   final Map<String, String> document;
 
   const DocumentTile({super.key, required this.document});
+
+  @override
+  DocumentTileState createState() => DocumentTileState();
+}
+
+class DocumentTileState extends State<DocumentTile> {
+  bool _tapped = false;
+
+  void _handleTap() {
+    setState(() {
+      _tapped = true;
+    });
+
+    Timer(const Duration(milliseconds: 200), () {
+      setState(() {
+        _tapped = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     String? icon;
 
-    switch (document['type']) {
+    switch (widget.document['type']) {
       case 'pdf':
         icon = 'assets/icons/pdf.svg';
         break;
@@ -25,21 +45,29 @@ class DocumentTile extends StatelessWidget {
         icon = 'assets/icons/unknown.svg';
     }
 
-    return Card(
-      elevation: 2.0,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(
-            icon,
-            width: 60,
-            height: 60,
-            color: theme.colorScheme.surfaceContainerHighest,
-            placeholderBuilder: (context) => const CircularProgressIndicator(),
-          ),
-          const SizedBox(height: 20),
-          Text(document['name']!, style: const TextStyle(fontSize: 16)),
-        ],
+    return GestureDetector(
+      onTap: _handleTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        color: _tapped
+            ? theme.colorScheme.surfaceContainerLowest
+            : theme.colorScheme.surface,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              icon,
+              width: 60,
+              height: 60,
+              color: theme.colorScheme.surfaceContainerHighest,
+              placeholderBuilder: (context) =>
+                  const CircularProgressIndicator(),
+            ),
+            const SizedBox(height: 20),
+            Text(widget.document['name']!,
+                style: const TextStyle(fontSize: 16)),
+          ],
+        ),
       ),
     );
   }
